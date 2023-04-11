809141d3a9c2193f3b3143b885e670b2cd0afba4
Akhand Pratap Singh <akhisin777@gmail.com>
2023-04-11 11:50:53 +0530

    commit

---
+++ Second class.py
@@ -0,0 +1,16 @@
+print("hello World") 
+print("apple", 'mango',"Banana")
+print("apple",'Mango',"Banana", sep="&")
+
+
+print("akhand", "saurabh", "tushar", sep=" ")
+
+print("akhand", "Saurabh", sep=" ", end='\n')
+
+print("akhand", "Saurabh", sep=" ", end=' ')
+
+
+print("akhand", "Saurabh", sep=" ", end='\n')
+
+
+print("hey i am Akhand")
---
+++ first.py.py
@@ -0,0 +1,42 @@
+#variable: variable is a called identifier or container.
+# 1- variable is never start with number.
+# 2- variable never contain special character except underscore( _ )
+# 3- Variable name must be readable.
+
+#rule.1
+
+a= 10
+b= 80
+print("a")
+
+a1= 10
+b1= 50
+print("a1")
+
+
+#rule.2
+
+a_b=80
+b_a=40
+print("a_b")
+
+
+akhand_age=20
+print("akhand_age")
+
+#rule.3
+
+#akhandpratapage=20  #not in readble form
+
+akhand_pratap_age=20
+print("akhand_pratap_age")
+
+#Akhand_Singh=50 #snake case (both are small letter )
+
+#akhandSingh=50 #camel case( start with small letter and the other word is capital letter )
+
+#AkhandSingh=50 #pascal case ( both letter is capital )
+
+print("welcome to new journey")
+print("let's start it with a smile")
+print("hello")
